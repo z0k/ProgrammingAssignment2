9495fa7f51eebbb3eb435f864e7276117256229f
z0k <zaraks@icloud.com>
2014-04-27 18:19:42 -0400

    Matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-
-
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
-
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    
+        i <- NULL                   # Set an object to hold the matrix inverse
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setMatrix <- function(inverse) i <<- inverse
+        getMatrix <- function() i
+        list(set = set, get = get,
+             setMatrix = setMatrix,
+             getMatrix = getMatrix)
+ 
  
 }
 
@@ -41,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getMatrix()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setMatrix(i)
+        i
+
 }
